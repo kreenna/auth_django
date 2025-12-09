f1e5ed6d54ab8d6bcfa2749880de74ea7a091624
Arina <rudenskaaarina@gmail.com>
2025-12-09 18:08:00 +0300

    views

--- auth_app/views.py
+++ auth_app/views.py
@@ -2,10 +2,12 @@ from datetime import datetime, timedelta
 
 from rest_framework import status
 from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import User, Session
+from .permissions import HasPermission
 from .serializers import UserRegisterSerializer, UserLoginSerializer, UserSerializer
 from .services import create_jwt_token
 
@@ -68,3 +70,63 @@ class LogoutView(APIView):
         token = request.META.get("HTTP_AUTHORIZATION", "").replace("Bearer ", "")
         Session.objects.filter(session_token=token).delete()
         return Response({"message": "Выход выполнен"}, status=status.HTTP_200_OK)
+
+
+class UserProfileView(APIView):
+    """Отображение профиля."""
+
+    def get(self, request):
+        """Обработка GET запросов."""
+        return Response(UserSerializer(request.user).data)
+
+    def patch(self, request):
+        """Обработка PATCH запросов."""
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
+        # проверяем корректность пароля
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteAccountView(APIView):
+    """Удаление аккаунта."""
+
+    def post(self, request):
+        """Обработка POST запросов."""
+        request.user.is_active = False
+        request.user.save()
+        # выход из аккаунта
+        token = request.META.get("HTTP_AUTHORIZATION", "").replace("Bearer ", "")
+        Session.objects.filter(session_token=token).delete()
+        return Response({"message": "Аккаунт удален"}, status=status.HTTP_200_OK)
+
+
+# Mock бизнес-ресурсы
+class ProductsView(APIView):
+    permission_classes = [IsAuthenticated, HasPermission]
+    element_name = "products"
+
+    def get(self, request):
+        """Обработка GET запросов."""
+        return Response({"products": [
+            {"id": 1, "name": "Product 1", "owner": request.user.id},
+            {"id": 2, "name": "Product 2", "owner": request.user.id}
+        ]})
+
+    def post(self, request):
+        """Обработка POST запросов."""
+        return Response({"message": "Продукт создан", "id": 3})
+
+
+class OrdersView(APIView):
+    """Отображение заказов."""
+    permission_classes = [IsAuthenticated, HasPermission]
+    element_name = "orders"
+
+    def get(self, request):
+        """Обработка GET запросов."""
+        return Response({"orders": [
+            {"id": 1, "product": "Product 1", "status": "pending"},
+            {"id": 2, "product": "Product 2", "status": "completed"}
+        ]})
